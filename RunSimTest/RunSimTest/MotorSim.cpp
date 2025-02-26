fa6ae406b7add863a3906edb83a7337ba86ee777
Ali majidi <a.m.majidi.62@gmail.com>
2025-02-26 19:44:12 +0100

    some change

--- RunSimTest/RunSimTest/MotorSim.cpp
+++ RunSimTest/RunSimTest/MotorSim.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <string>
+using namespace std;
 class MotorSim {
 private:
     double acceleration;
@@ -29,20 +30,20 @@ public:
     }
 
     std::string getStatus() const {
-        return "Current Speed: " + std::to_string(currentSpeed) + " m/s, Max Speed: " + std::to_string(maxSpeed) + " m/s";
+        return "Current Speed: " + to_string(currentSpeed) + " m/s, Max Speed: " + std::to_string(maxSpeed) + " m/s";;
     }
 };
 
-int main() {
-    MotorSim motor;
-    motor.setAcceleration(2.0); // 2 m/s^2
-    motor.setMaxSpeed(10.0); // 10 m/s
-
-    motor.updateSpeed(1.0); // Update speed after 1 second
-    std::cout << motor.getStatus() << std::endl;
-
-    motor.updateSpeed(2.0); // Update speed after another 2 seconds
-    std::cout << motor.getStatus() << std::endl;
-
-    return 0;
-}
+//int main() {
+//    MotorSim motor;
+//    motor.setAcceleration(2.0); // 2 m/s^2
+//    motor.setMaxSpeed(10.0); // 10 m/s
+//
+//    motor.updateSpeed(1.0); // Update speed after 1 second
+//    std::cout << motor.getStatus() << std::endl;
+//
+//    motor.updateSpeed(2.0); // Update speed after another 2 seconds
+//    std::cout << motor.getStatus() << std::endl;
+//
+//    return 0;
+//}
